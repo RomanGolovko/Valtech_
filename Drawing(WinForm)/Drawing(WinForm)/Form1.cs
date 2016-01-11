9d4bd8cad76fb44eb08cc8b20115d0d24927e7bd
Roman Golovko <luck-8@yandex.ru>
2016-01-11 16:20:49 +0200

    Some new features

--- Drawing(WinForm)/Drawing(WinForm)/Form1.cs
+++ Drawing(WinForm)/Drawing(WinForm)/Form1.cs
@@ -87,3 +87,4 @@ namespace Drawing_WinForm_
         }
     }
 }
+
---
+++ Drawing(WinForm)/Drawing(WinForm)/Formats/Abstract/IFormat.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace Drawing_WinForm_.Formats.Abstract
+{
+    public interface IFormat
+    {
+        void Save(string path, Image image);
+        Image Load(string path);
+    }
+}
