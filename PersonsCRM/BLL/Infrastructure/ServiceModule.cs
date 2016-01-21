a1d8891b33a8cd04794cdf8a40189bc347b7ee49
Roman Golovko <luck-8@yandex.ru>
2016-01-21 16:35:58 +0200

    Add dapper

--- PersonsCRM/BLL/Infrastructure/ServiceModule.cs
+++ PersonsCRM/BLL/Infrastructure/ServiceModule.cs
@@ -1,6 +1,7 @@
 ﻿using DAL.DB.Abstract;
 using DAL.DB.Concrete.EF;
 using DAL.DB.Concrete.LiteDb;
+using DAL.DB.Concrete.MSSQL.Dapper;
 using DAL.Entities;
 using Ninject.Modules;
 
@@ -9,7 +10,7 @@ namespace BLL.Infrastructure
     public class ServiceModule : NinjectModule
     {
         private readonly string _connectionString;
-
+        
         public ServiceModule(string connection)
         {
             _connectionString = connection;
@@ -18,7 +19,8 @@ namespace BLL.Infrastructure
         public override void Load()
         {
             //Bind<IRepository<Person>>().To<LiteDbRepository>();
-            Bind<IRepository<Person>>().To<EfRepository>().WithConstructorArgument(_connectionString);
+            //Bind<IRepository<Person>>().To<EfRepository>().WithConstructorArgument(_connectionString);
+            Bind<IRepository<Person>>().To<DapperRepository>().WithConstructorArgument(_connectionString);
         }
     }
 }
---
+++ PersonsCRM/DAL/DB/Concrete/MSSQL/Dapper/DapperRepository.cs
@@ -0,0 +1,70 @@
+﻿using DAL.DB.Abstract;
+using DAL.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DAL.DB.Concrete.MSSQL.Dapper
+{
+    public class DapperRepository : IRepository<Person>
+    {
+        string connectionString; /*= ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;*/
+
+        public DapperRepository(string connection)
+        {
+            connectionString = connection;
+        }
+
+        public Person Get(int id)
+        {
+            Person person = null;
+
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                person = db.Query<Person>("SELECT * FROM Persons WHERE Id = @id", new { id }).FirstOrDefault();
+            }
+            return person;
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            var persons = new List<Person>();
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                persons = db.Query<Person>("SELECT * FROM Persons").ToList();
+            }
+
+            return persons;
+        }
+
+        public void Create(Person item)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "";
+
+                if (item.Id == 0)
+                {
+                    sqlQuery = "INSERT INTO Persons (FirstName, LastName, Age) VALUES(@FirstName, @LastName, @Age)";
+                }
+                else
+                {
+                    sqlQuery = "UPDATE Persons SET FirstName = @FirstName, LastName = @LastName, Age = @Age";
+                }
+
+                db.Execute(sqlQuery, item);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "DELETE FROM Persons WHERE Id = @id";
+                db.Execute(sqlQuery, new { id });
+            }
+        }
+    }
+}
